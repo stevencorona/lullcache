a418a37d1ad6fb507d312f33de70805ee985eee3
Steve Corona <stevecorona@me.com>
2014-05-20 05:34:50 -0700

    use textproto; reimplement protocol parsing

--- server.go
+++ server.go
@@ -1,72 +1,87 @@
 package main
 
 import (
-  "net"
-  "fmt"
-  "log"
-  "strings"
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"net/textproto"
+	"strings"
 )
 
 func NewCacheServer(address string) {
-  listener, err := net.Listen("tcp", address)
+	listener, err := net.Listen("tcp", address)
 
-  if err != nil {
-    log.Fatal("Error creating TCP socket", address, err.Error())
-  }
+	if err != nil {
+		log.Fatal("Error creating TCP socket", address, err.Error())
+	}
 
-  // Safe to close the listener after error checking
-  defer listener.Close()
+	// Safe to close the listener after error checking
+	defer listener.Close()
 
-  // Loop, accept, push work into a goroutine
-  for {
-    conn, err := listener.Accept()
+	// Loop, accept, push work into a goroutine
+	for {
+		conn, err := listener.Accept()
 
-    if err != nil {
-      log.Println("Connection error from accept", err.Error())
-    }
+		if err != nil {
+			log.Println("Connection error from accept", err.Error())
+		}
 
-    // TODO: Use a pool of Goroutines
-    go CacheServerRawHandler(conn)
-  }
+		// TODO: Use a pool of Goroutines
+		go CacheServerRawHandler(conn)
+	}
 }
 
-var setCommand  = []byte("s")[0]
-var getCommand  = []byte("g")[0]
-var quitCommand = []byte("q")[0]
+var cacheData = make(map[string]string)
 
 func CacheServerRawHandler(conn net.Conn) {
 
-  // TODO: Use a circular buffer or a static allocation
-  // TODO: 1024 buffer is probably not enough
-  buffer := make([]byte, 1024)
-  defer conn.Close()
+	reader := bufio.NewReader(conn)
+	protocol := textproto.NewReader(reader)
 
-  // Loop and read, parsing for commands along the way
-  for {
-    count, err := conn.Read(buffer)
+	defer conn.Close()
 
-    if err != nil {
-      log.Println("Error reading from client", err.Error())
-      return
-    }
+	// Loop and read, parsing for commands along the way
+	for {
 
-    command := strings.TrimSpace(string(buffer))
-    fmt.Println("got ", count, "bytes, ", command)
+		line, err := protocol.ReadLine()
 
-    if buffer[0] == quitCommand {
-      return
-    }
+		if err != nil {
+			log.Println("Error reading from client", err.Error())
+			return
+		}
 
-    if buffer[0] == setCommand {
-      log.Println("set")
-      conn.Write([]byte("STORED\r\n"))
-    }
+		fmt.Println("got line: ", line)
 
-    if buffer[0] == getCommand {
-      log.Println("get")
-      conn.Write([]byte("VALUE key flags bytes\r\n"))
-      conn.Write([]byte("Some data\r\n"))
-    }
+		tokens := strings.Split(line, " ")
 
-  }
+		// TODO: Guard
+		command := tokens[0]
+
+		if command == "quit" {
+			return
+		}
+
+		if command == "get" {
+			// TODO: Guard this
+			key := tokens[1]
+
+			conn.Write([]byte(cacheData[key]))
+			conn.Write([]byte("VALUE key flags bytes\r\n"))
+			conn.Write([]byte("data blog\r\n"))
+			conn.Write([]byte("END\r\n"))
+		}
+
+		if command == "set" {
+			key := tokens[1]
+			//flags := tokens[2]
+			//exptime := tokens[3]
+			//bytes := tokens[4]
+
+			cacheData[key] = "data"
+
+			conn.Write([]byte("STORED\r\n"))
+		}
+
+	}
 }
