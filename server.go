ae7d163560f7984489d35d8631108de958bb11a8
Steve Corona <stevecorona@me.com>
2014-05-22 07:54:50 -0700

    add multiget support

--- server.go
+++ server.go
@@ -76,17 +76,18 @@ func CacheServerRawHandler(conn net.Conn) {
 		}
 
 		if command == "get" {
-			// TODO: Guard this
-			key := tokens[1]
 
-			if item, ok := cacheData[key]; ok {
+			for _, key := range tokens[1:] {
 
-				if timestamp > item.Exptime {
-					log.Println("expiring key:", key)
-					delete(cacheData, key)
-				} else {
-					out := fmt.Sprintf("VALUE %s %s %d\r\n%s\r\n", key, item.Flag, item.Exptime, item.Value)
-					conn.Write([]byte(out))
+				if item, ok := cacheData[key]; ok {
+
+					if timestamp > item.Exptime {
+						log.Println("expiring key:", key)
+						delete(cacheData, key)
+					} else {
+						out := fmt.Sprintf("VALUE %s %s %d\r\n%s\r\n", key, item.Flag, item.Exptime, item.Value)
+						conn.Write([]byte(out))
+					}
 				}
 			}
 
