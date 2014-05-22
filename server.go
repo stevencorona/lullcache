fcbabb6e8e1aecb5b013d484d12cea01f37fd16d
Steve Corona <stevecorona@me.com>
2014-05-22 07:51:31 -0700

    document the seconds/timestamp behavior

--- server.go
+++ server.go
@@ -105,6 +105,8 @@ func CacheServerRawHandler(conn net.Conn) {
 			exptime, _ := strconv.ParseInt(tokens[3], 10, 32)
 			length, _ := strconv.ParseInt(tokens[4], 10, 32)
 
+			// if the exptime is less than 30 days, it's probably just number of
+			// seconds and not a timestamp, so add it to unix time.
 			if exptime < 2592000 {
 				exptime += timestamp
 			}
