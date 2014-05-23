61fea1831f97336210c125cc3f91aa62d775c985
Steve Corona <stevecorona@me.com>
2014-05-23 13:38:56 -0700

    move set to it's own func

--- server.go
+++ server.go
@@ -95,34 +95,7 @@ func (s *CacheServer) CacheServerRawHandler(conn net.Conn) {
 		}
 
 		if command == "set" || command == "add" {
-
-			if len(tokens) != 5 {
-				conn.Write([]byte("Error"))
-				return
-			}
-
-			key := tokens[1]
-			flags := tokens[2]
-			exptime, _ := strconv.ParseInt(tokens[3], 10, 32)
-			length, _ := strconv.ParseInt(tokens[4], 10, 32)
-
-			// if the exptime is less than 30 days, it's probably just number of
-			// seconds and not a timestamp, so add it to unix time.
-			if exptime < 2592000 {
-				exptime += timestamp
-			}
-
-			// Guard this
-			bytes := make([]byte, length)
-			io.ReadFull(reader, bytes)
-
-			fmt.Println("got this:", string(bytes))
-
-			s.Store.Lock()
-			s.Store.Data[key] = CacheItem{exptime, bytes, flags}
-			s.Store.Unlock()
-
-			conn.Write([]byte("STORED\r\n"))
+			s.CommandSet(conn, reader, tokens, timestamp)
 		}
 	}
 }
@@ -149,4 +122,34 @@ func (s *CacheServer) CommandGet(conn net.Conn, tokens []string, timestamp int64
 	}
 
 	conn.Write([]byte("END\r\n"))
+}
+
+func (s *CacheServer) CommandSet(conn net.Conn, reader *bufio.Reader, tokens []string, timestamp int64) {
+	if len(tokens) != 5 {
+		conn.Write([]byte("Error"))
+		return
+	}
+
+	key := tokens[1]
+	flags := tokens[2]
+	exptime, _ := strconv.ParseInt(tokens[3], 10, 32)
+	length, _ := strconv.ParseInt(tokens[4], 10, 32)
+
+	// if the exptime is less than 30 days, it's probably just number of
+	// seconds and not a timestamp, so add it to unix time.
+	if exptime < 2592000 {
+		exptime += timestamp
+	}
+
+	// Guard this
+	bytes := make([]byte, length)
+	io.ReadFull(reader, bytes)
+
+	fmt.Println("got this:", string(bytes))
+
+	s.Store.Lock()
+	s.Store.Data[key] = CacheItem{exptime, bytes, flags}
+	s.Store.Unlock()
+
+	conn.Write([]byte("STORED\r\n"))
 }
