302615d4aa19f4393b048b6affa1eefdd906fb4a
Steve Corona <stevecorona@me.com>
2014-05-28 05:51:40 -0700

    implement the touch command

--- server.go
+++ server.go
@@ -126,6 +126,10 @@ func (s *CacheServer) CacheServerRawHandler(conn net.Conn) {
 
 		}
 
+		if command == "touch" {
+			s.CommandTouch(conn, tokens, timestamp)
+		}
+
 	}
 }
 
@@ -151,6 +155,29 @@ func (s *CacheServer) CommandGet(conn net.Conn, tokens []string, timestamp int64
 	}
 
 	conn.Write([]byte("END\r\n"))
+}
+
+func (s *CacheServer) CommandTouch(conn net.Conn, tokens []string, timestamp int64) {
+
+	key := tokens[1]
+	exptime, _ := strconv.ParseInt(tokens[2], 10, 32)
+
+	// if the exptime is less than 30 days, it's probably just number of
+	// seconds and not a timestamp, so add it to unix time.
+	if exptime != 0 && exptime < 2592000 {
+		exptime += timestamp
+	}
+
+	if item, ok := s.Store.Data[key]; ok {
+
+		s.Store.Lock()
+		item.Exptime += exptime
+		s.Store.Data[key] = item
+		s.Store.Unlock()
+		conn.Write([]byte("TOUCHED\r\n"))
+	} else {
+		conn.Write([]byte("NOT FOUND\r\n"))
+	}
 }
 
 func (s *CacheServer) CommandSet(conn net.Conn, reader *bufio.Reader, tokens []string, timestamp int64) {
