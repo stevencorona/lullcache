e364c0f4beb481dc7fef4973d02e4993e0b413b4
Steve Corona <stevecorona@me.com>
2014-05-30 07:57:35 -0700

    move get to its own file

---
+++ get.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func (s *CacheServer) CommandGet(conn net.Conn, tokens []string, timestamp int64) {
+	for _, key := range tokens[1:] {
+
+		s.Store.RLock()
+		if item, ok := s.Store.Data[key]; ok {
+
+			s.Store.RUnlock()
+
+			if timestamp != 0 && timestamp > item.Exptime {
+				log.Println("expiring key:", key)
+
+				s.Store.Lock()
+				delete(s.Store.Data, key)
+				s.Store.Unlock()
+			} else {
+				out := fmt.Sprintf("VALUE %s %s %d\r\n%s\r\n", key, item.Flag, item.Exptime, item.Value)
+				conn.Write([]byte(out))
+			}
+		}
+	}
+
+	conn.Write([]byte("END\r\n"))
+}
--- server.go
+++ server.go
@@ -148,30 +148,6 @@ func (s *CacheServer) RawHandler(conn net.Conn) {
 	}
 }
 
-func (s *CacheServer) CommandGet(conn net.Conn, tokens []string, timestamp int64) {
-	for _, key := range tokens[1:] {
-
-		s.Store.RLock()
-		if item, ok := s.Store.Data[key]; ok {
-
-			s.Store.RUnlock()
-
-			if timestamp != 0 && timestamp > item.Exptime {
-				log.Println("expiring key:", key)
-
-				s.Store.Lock()
-				delete(s.Store.Data, key)
-				s.Store.Unlock()
-			} else {
-				out := fmt.Sprintf("VALUE %s %s %d\r\n%s\r\n", key, item.Flag, item.Exptime, item.Value)
-				conn.Write([]byte(out))
-			}
-		}
-	}
-
-	conn.Write([]byte("END\r\n"))
-}
-
 func (s *CacheServer) CommandTouch(conn net.Conn, tokens []string, timestamp int64) {
 
 	key := tokens[1]
