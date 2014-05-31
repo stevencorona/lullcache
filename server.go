66cb6f5c09be85101ba777a363a9278577d7cad7
Steve Corona <stevecorona@me.com>
2014-05-31 13:42:37 -0700

    move add to own file

--- server.go
+++ server.go
@@ -146,27 +146,6 @@ func (s *CacheServer) RawHandler(conn net.Conn) {
 	}
 }
 
-func (s *CacheServer) CommandAdd(conn net.Conn, reader *bufio.Reader, tokens []string, timestamp int64) {
-
-	if len(tokens) != 5 {
-		conn.Write([]byte("Error"))
-		return
-	}
-
-	key := tokens[1]
-
-	s.Store.RLock()
-
-	if _, ok := s.Store.Data[key]; !ok {
-		s.Store.RUnlock()
-		s.CommandSet(conn, reader, tokens, timestamp)
-	} else {
-		s.Store.RUnlock()
-		conn.Write([]byte("NOT STORED\r\n"))
-	}
-
-}
-
 func (s *CacheServer) CommandReplace(conn net.Conn, reader *bufio.Reader, tokens []string, timestamp int64) {
 
 	if len(tokens) != 5 {
